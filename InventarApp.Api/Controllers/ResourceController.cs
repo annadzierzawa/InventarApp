809b18c000199574f37a9ef781205e11bbd7bca4
Anna Dzierżawa <annadzierzawa@gmail.com>
2020-10-18 15:33:51 +0200

    soome changes

---
+++ InventarApp.Api/Controllers/ResourceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventarApp.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ResourceController : ControllerBase
+    {
+        private readonly IResourceService _resourceService;
+        public ResourceController(IResourceService resourceService)
+        {
+            _resourceService = resourceService;
+        }
+    }
+}
---
+++ InventarApp.Application/Services/ResourceService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventarApp.Application.Services
+{
+    public class ResourceService: IResourceService
+    {
+    }
+}
